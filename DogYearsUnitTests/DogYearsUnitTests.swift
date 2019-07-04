295556e05d48e78593712396f67bc122cd232d7e
Elias Gabriel <elias.gabrielfn12@gmail.com>
2019-07-03 22:00:04 -0400

    Getting started
    Signed-off-by: Elias Gabriel <elias.gabrielfn12@gmail.com>

---
+++ DogYearsUnitTests/DogYearsUnitTests.swift
@@ -0,0 +1,33 @@
+//
+//  DogYearsUnitTests.swift
+//  DogYearsUnitTests
+//
+//  Created by Elias Gabriel dos Santos Correa on 03/07/19.
+//  Copyright © 2019 Razeware. All rights reserved.
+//
+
+import XCTest
+
+class DogYearsUnitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
