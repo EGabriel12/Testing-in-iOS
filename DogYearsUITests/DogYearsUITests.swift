1e3d261f5608908415ac4de4fa2724e356e74679
Elias Gabriel <elias.gabrielfn12@gmail.com>
2019-07-05 22:00:48 -0400

    Refactored UI test
    Signed-off-by: Elias Gabriel <elias.gabrielfn12@gmail.com>

--- DogYearsUITests/DogYearsUITests.swift
+++ DogYearsUITests/DogYearsUITests.swift
@@ -10,6 +10,8 @@ import XCTest
 
 class DogYearsUITests: XCTestCase {
 
+    private var app: XCUIApplication!
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -17,7 +19,8 @@ class DogYearsUITests: XCTestCase {
         continueAfterFailure = false
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        app = XCUIApplication()
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -27,7 +30,14 @@ class DogYearsUITests: XCTestCase {
     }
 
     func testExample() {
-        XCUIApplication().navigationBars["Master"].buttons["Menu"].tap()
+        //XCUIApplication().navigationBars["Master"].buttons["Menu"].tap()
+        let navBar = app.navigationBars["Master"] // Navigations bars na controller Menu
+        let button = navBar.buttons["Menu"]
+        button.tap()
+        XCTAssertFalse(navBar.exists, "The old navigation bar no longer exists")
+        let nav2 = app.navigationBars["Menu"]
+        XCTAssert(nav2.exists, "The new navigation bar does not exist")
+        
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
